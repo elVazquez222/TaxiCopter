0ed3ba17f34c6eddabdba7a4b4e1e4ea32458438
jose.guerrerovazquez <joseguerrerovazquez@bundeswehr.org>
2023-02-03 17:26:38 +0100

    create destinations.js

---
+++ destinations.js
@@ -0,0 +1,22 @@
+const destinations = [{
+    id: 1,
+    width: 200,
+    height: 300,
+    x: 500
+  },
+  {
+    id: 2,
+    width: 300,
+    height: 700,
+    x: 900
+  },
+  {
+    id: 3,
+    width: 400,
+    height: 100,
+    x: 100,
+    y: 400
+  },
+];
+
+export { destinations }
