504006986f6ac7d428760b38af3467dc9f2ad455
jose.guerrerovazquez <joseguerrerovazquez@bundeswehr.org>
2023-02-06 10:12:41 +0100

    improve copter design a bit

--- assets/destinations.js
+++ assets/destinations.js
@@ -1,21 +1,29 @@
 const destinations = [{
     id: 1,
-    width: 200,
+    width: 300,
     height: 300,
-    x: 500
+    x: 400
   },
   {
     id: 2,
-    width: 300,
-    height: 700,
-    x: 900
+    width: 400,
+    height: 300,
+    x: 100,
+    y: 400
   },
   {
     id: 3,
-    width: 400,
-    height: 100,
-    x: 100,
-    y: 400
+    width: 300,
+    height: 500,
+    x: 900,
+    y: 600
+  },
+  {
+    id: 4,
+    width: 600,
+    height: 300,
+    x: 1400,
+    y: 500
   },
 ];
 
